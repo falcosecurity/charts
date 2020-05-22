3a3f8ce080abadd1b7e8914ea4ba6f653c44d72c
Spencer Krum <nibz@spencerkrum.com>
2020-05-22 19:19:33 +0000

    feat(release): Add release script for helm hub
    Signed-off-by: Spencer Krum <nibz@spencerkrum.com>

---
+++ release.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ ! -d ../gh-pages/.git ]; then
+    echo "Need to set up 'gh-pages' clone in directory above for this script to work"
+    echo "Run:"
+    echo "cd .."
+    echo "git clone git@github.com/falcosecurity/charts gh-pages"
+    echo "cd gh-pages"
+    echo "git checkout gh-pages"
+    exit 1
+fi
+
+helm package falco
+mv *.tgz ../gh-pages/
+pushd ../gh-pages
+git checkout gh-pages
+
+#TODO rename to falcosecurity before merge
+helm repo index . --url https://nibalizer.github.com/charts
+
+# Create json representation (because my browser wont render yaml)
+cat index.yaml | python -c 'import sys, yaml, json; y=yaml.load(sys.stdin.read()); print json.dumps(y)' | jq '.' > index.json
+
+git add .
+
+git commit -m "Helm chart/repo release $(date +%F)"
+
+git push origin gh-pages
